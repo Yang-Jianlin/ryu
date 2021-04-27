03cfeb9bc39b5241e52216ca054ba36aa8448f98
Yang-Jianlin <1494237426@qq.com>
2021-04-27 22:06:57 +0800

    提交了monitor

--- ryu/app/test.py
+++ ryu/app/test.py
@@ -1,18 +1,6 @@
-mac_table = {}
-dpid = input('enter dpid:')
-
-mac_table.setdefault(dpid, {})
-print(mac_table)
-
-src = input('enter src:')
-dst = input('enter dst:')
-inport = input('enter inport:')
-mac_table[dpid][src] = inport
-mac_table[dpid][dst] = inport
-print(mac_table)
-
-print(mac_table[dpid])
-for i in mac_table[dpid]:
-    print(i)
-for j in mac_table[dpid].values():
-    print(j)
+print('datapath     port receive-packets  receive-bytes  '
+      'receive-error  receive-drop  send-packets  send-bytes  '
+      'send-error  send-drop')
+print('---------    ---- ---------------  -------------  -------------  '
+      '-----------   -----------   -----------  ---------  ---------')
+print('%016x %8x %17s %8x %8d %8d')
